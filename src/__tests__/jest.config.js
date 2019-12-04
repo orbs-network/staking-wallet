971906ff585955a3268f0bdfc1b19b1e46cb36bf
Or Lavy <orlavyil@gmail.com>
2019-12-04 16:24:05 +0200

    Fixes jest config to allow mocking of assets

--- src/__tests__/jest.config.js
+++ src/__tests__/jest.config.js
@@ -6,8 +6,13 @@
  * The above notice should be included in all copies or substantial portions of the software.
  */
 
+// DEV_NOTE : For loading of assets : https://jestjs.io/docs/en/webpack.html
+
 module.exports = {
   testRegex: '.*test.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+  },
   setupFilesAfterEnv: ['jest-expect-message'],
 };
