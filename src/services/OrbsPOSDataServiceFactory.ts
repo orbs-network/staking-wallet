68927fe3a4d9baf8fd3866ef507af3802df0c0ac
Or Lavy <orlavyil@gmail.com>
2019-12-08 11:47:10 +0200

    Fixes connection to infura to be WSS

--- src/services/OrbsPOSDataServiceFactory.ts
+++ src/services/OrbsPOSDataServiceFactory.ts
@@ -3,9 +3,10 @@ import Web3 from 'web3';
 import { BuildOrbsClient } from './OrbsClientFactory';
 
 const ETHEREUM_PROVIDER_URL = 'https://mainnet.infura.io/v3/3fe9b03bd8374639809addf2164f7287';
+const ETHEREUM_PROVIDER_WS = 'wss://mainnet.infura.io/ws/v3/3fe9b03bd8374639809addf2164f7287';
 
 export function buildOrbsPOSDataService(): IOrbsPOSDataService {
-  const web3 = new Web3(new Web3.providers.HttpProvider(ETHEREUM_PROVIDER_URL));
+  const web3 = new Web3(new Web3.providers.WebsocketProvider(ETHEREUM_PROVIDER_WS));
   const orbsClient = BuildOrbsClient();
   return orbsPOSDataServiceFactory(web3, orbsClient);
 }
