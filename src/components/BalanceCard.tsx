2f81011126cbcc4220b7d40705ac8b1e740b3aa8
Or Lavy <orlavyil@gmail.com>
2019-12-22 17:49:47 +0200

    Fixes styling of 'BalanceCard'

--- src/components/BalanceCard.tsx
+++ src/components/BalanceCard.tsx
@@ -7,6 +7,11 @@ const StyledGrid = styled('div')(styledProps => {
   return {
     backgroundColor: styledProps.theme.palette.primary.main,
     padding: '1em',
+    paddingRight: '1.5em',
+    paddingLeft: '1.5em',
+
+    display: 'flex',
+    flexDirection: 'column',
   };
 });
 
@@ -21,20 +26,16 @@ interface IProps {
 // TODO : O.L : Change this component to use Card.
 
 export const BalanceCard: React.FC<IProps> = (props: IProps) => {
-  const {
-    title, amount,
-    actionButtonTitle,
-    actionButtonActive,
-    onActionButtonPressed,
-  } = props;
+  const { title, amount, actionButtonTitle, actionButtonActive, onActionButtonPressed } = props;
 
   return (
     <StyledGrid>
       <Typography variant={'caption'}>{title}</Typography>
-      <br />
+
       <CommonDivider />
+
       <Typography variant={'h6'}>{amount.toLocaleString()}</Typography>
-      <br />
+
       <Button color={'secondary'} variant={'contained'} disabled={!actionButtonActive} onClick={onActionButtonPressed}>
         {actionButtonTitle}
       </Button>
