dc3ac84f20ef6def7d49954b854a2250267863f7
Or Lavy <orlavyil@gmail.com>
2019-12-22 19:15:39 +0200

    Fixes style of model and stepper

--- src/components/modals/modalsConfiguration.ts
+++ src/components/modals/modalsConfiguration.ts
@@ -1,3 +1,7 @@
+// DEV_NOTE : O.L : For now the theme properties here area hard coded. we can consider using 'styled components' later if needed.
+import { baseTheme } from '../../theme/Theme';
+import Color from 'color';
+
 export const MODAL_CSTYLE_ENTERED_CONTENTS = {
   content: {
     top: '50%',
@@ -6,5 +10,10 @@ export const MODAL_CSTYLE_ENTERED_CONTENTS = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
+    backgroundColor: Color(baseTheme.palette.primary.main).fade(1).rgb().string(),
+    border: 'none',
+  },
+  overlay: {
+    backgroundColor: Color(baseTheme.palette.primary.main).rgb().fade(0.05).string(),
   },
 };
--- src/wizards/staking/StakingWizard.tsx
+++ src/wizards/staking/StakingWizard.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Step, StepLabel, Stepper } from '@material-ui/core';
 import { useNumber } from 'react-hanger';
+import styled from 'styled-components';
+
+const StyledStepper = styled(Stepper)(styledProps => ({
+  backgroundColor: 'rgba(0, 0, 0, 0)',
+}));
 
 export const StakingWizard: React.FC = () => {
   const activeStep = useNumber(0);
 
   return (
     <div>
-      <Stepper activeStep={activeStep.value}>
+      <StyledStepper activeStep={activeStep.value} alternativeLabel>
         <Step>
           <StepLabel>Staking your tokens</StepLabel>
         </Step>
@@ -31,7 +36,7 @@ export const StakingWizard: React.FC = () => {
         <Step>
           <StepLabel>Success selecting guardian</StepLabel>
         </Step>
-      </Stepper>
+      </StyledStepper>
     </div>
   );
 };
