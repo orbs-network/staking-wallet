b888175965f416399a8a5107e39c0f62f44fec1d
Gil Amran <gilamran@gmail.com>
2020-01-06 15:46:49 +0200

    showing guardians

--- ganache-env/migrations/4_guardians.js
+++ ganache-env/migrations/4_guardians.js
@@ -5,7 +5,10 @@ module.exports = async function(deployer, network, accounts) {
   const registrationMinTime = 60 * 3;
 
   await deployer.deploy(OrbsGuardians, registrationDepositWei, registrationMinTime);
-
-  const instance = await OrbsGuardians.new(10, 0);
-  await instance.register('Gil', 'www.gilamran.com', { from: accounts[1], value: 10 });
+  const instance = await OrbsGuardians.deployed();
+  await instance.register('Guardian 1', 'www.guardian-1.com', { from: accounts[1], value: registrationDepositWei });
+  await instance.register('Guardian 2', 'www.guardian-2.com', { from: accounts[2], value: registrationDepositWei });
+  await instance.register('Guardian 3', 'www.guardian-3.com', { from: accounts[3], value: registrationDepositWei });
+  await instance.register('Guardian 4', 'www.guardian-4.com', { from: accounts[4], value: registrationDepositWei });
+  await instance.register('Guardian 5', 'www.guardian-5.com', { from: accounts[5], value: registrationDepositWei });
 };
--- src/sections/GuardiansSection.tsx
+++ src/sections/GuardiansSection.tsx
@@ -4,11 +4,13 @@ import { observer } from 'mobx-react';
 import SecurityIcon from '@material-ui/icons/Security';
 import { SectionHeader } from '../components/structure/SectionHeader';
 import { Section } from '../components/structure/Section';
+import { Guardians } from '../components/Guardians';
 
 export const GuardiansSection = observer(() => {
   return (
     <Section>
       <SectionHeader title={'ALL GUARDIANS'} icon={SecurityIcon} />
+      <Guardians/>
     </Section>
   );
 });
