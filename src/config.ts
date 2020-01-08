4f9805f6a4caf821665307f335f88e1e2d793fa8
Gil Amran <gilamran@gmail.com>
2020-01-08 12:35:32 +0200

    Added voting contract to the ganache-env

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import { IOrbsPosContractsAddresses } from 'orbs-pos-data';
+
 /**
  * Copyright 2019 the prism authors
  * This file is part of the prism library in the Orbs project.
@@ -6,28 +8,29 @@
  * The above notice should be included in all copies or substantial portions of the software.
  */
 
-
 ////////////// CONFIG VARIABLES ///////////////
-const config = {
-  contractsAddressesOverride: {
-    stakingContract: undefined,
-    erc20Contract: undefined,
-    guardiansContract: undefined,
-  },
+interface IConfig {
+  contractsAddressesOverride: Partial<IOrbsPosContractsAddresses & { stakingContract: string }>;
+  ETHEREUM_PROVIDER_WS: string;
+}
+const config: IConfig = {
+  contractsAddressesOverride: {},
   ETHEREUM_PROVIDER_WS: 'wss://mainnet.infura.io/ws/v3/3fe9b03bd8374639809addf2164f7287',
 };
-  
+
 // Webpack will remove this section on production build //
-if (process.env.NODE_ENV !== 'production') { 
+if (process.env.NODE_ENV !== 'production') {
   config.ETHEREUM_PROVIDER_WS = 'ws://localhost:8545';
 
   const OrbsGuardiansContractJSON = require('../ganache-env/build/contracts/OrbsGuardians.json');
   const OrbsTokenContractJSON = require('../ganache-env/build/contracts/OrbsToken.json');
   const StakingContractJSON = require('../ganache-env/build/contracts/StakingContract.json');
+  const VotingContractJSON = require('../ganache-env/build/contracts/OrbsVoting.json');
 
   config.contractsAddressesOverride.stakingContract = StakingContractJSON.networks['5777'].address;
   config.contractsAddressesOverride.erc20Contract = OrbsTokenContractJSON.networks['5777'].address;
   config.contractsAddressesOverride.guardiansContract = OrbsGuardiansContractJSON.networks['5777'].address;
-}
-  
+  config.contractsAddressesOverride.votingContract = VotingContractJSON.networks['5777'].address;
+} 
+
 export default config;
--- src/services/OrbsPOSDataServiceFactory.ts
+++ src/services/OrbsPOSDataServiceFactory.ts
@@ -8,24 +8,5 @@ export function buildOrbsPOSDataService(): IOrbsPOSDataService {
   const web3 = new Web3(new Web3.providers.WebsocketProvider(config.ETHEREUM_PROVIDER_WS));
   const orbsClient = BuildOrbsClient();
 
-  // eslint-disable-next-line @typescript-eslint/no-use-before-define
-  const overridingAddresses = buildContractAddressesOverridingObject();
-
-  return orbsPOSDataServiceFactory(web3, orbsClient, overridingAddresses);
-}
-
-function buildContractAddressesOverridingObject(): Partial<IOrbsPosContractsAddresses> {
-  const { contractsAddressesOverride } = config;
-
-  const overridingAddresses: Partial<IOrbsPosContractsAddresses> = {};
-
-  if (contractsAddressesOverride.erc20Contract) {
-    overridingAddresses.erc20Contract = contractsAddressesOverride.erc20Contract;
-  }
-
-  if (contractsAddressesOverride.guardiansContract) {
-    overridingAddresses.guardiansContract = contractsAddressesOverride.guardiansContract;
-  }
-
-  return overridingAddresses;
+  return orbsPOSDataServiceFactory(web3, orbsClient, config.contractsAddressesOverride);
 }
