3db09b6c6cb4beef81a73f9caefa64ab58aa4594
Gil Amran <gil@orbs.com>
2020-01-09 15:03:00 +0200

    Usng the latest version of orbsPOS data

--- src/services/OrbsPOSDataServiceFactory.ts
+++ src/services/OrbsPOSDataServiceFactory.ts
@@ -1,8 +1,8 @@
-import { orbsPOSDataServiceFactory, IOrbsPOSDataService, IOrbsPosContractsAddresses } from 'orbs-pos-data';
+import { IOrbsPOSDataService, orbsPOSDataServiceFactory } from 'orbs-pos-data';
 import Web3 from 'web3';
+import config from '../config';
 import { BuildOrbsClient } from './OrbsClientFactory';
 
-import config from '../config';
 
 export function buildOrbsPOSDataService(): IOrbsPOSDataService {
   const web3 = new Web3(new Web3.providers.WebsocketProvider(config.ETHEREUM_PROVIDER_WS));
