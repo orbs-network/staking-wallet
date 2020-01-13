87798c4671b4eaddbcf5e62cfc59c177d4ae0cfd
Gil Amran <gil@orbs.com>
2020-01-13 12:45:25 +0200

    Added guardian's website as global

--- src/components/GuardiansTable.tsx
+++ src/components/GuardiansTable.tsx
@@ -9,6 +9,8 @@ import styled from 'styled-components';
 const asPercent = (num: number) =>
   (num * 100).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 2 }) + '%';
 
+const getWebsiteAddress = (url: string) => (url.toLowerCase().indexOf('http') === 0 ? url : `http://${url}`);
+
 const NameBox = styled.div`
   display: flex;
   align-items: center;
@@ -52,7 +54,16 @@ export const GuardiansTable = React.memo<IProps>(({ guardians, onGuardianSelect,
                 </NameBox>
               </TableCell>
               <TableCell data-testid={`guardian-${idx + 1}-address`}>{g.address}</TableCell>
-              <TableCell data-testid={`guardian-${idx + 1}-website`}>{g.website}</TableCell>
+              <TableCell>
+                <a
+                  data-testid={`guardian-${idx + 1}-website`}
+                  href={getWebsiteAddress(g.website)}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <img src='/assets/globe.svg' />
+                </a>
+              </TableCell>
               <TableCell data-testid={`guardian-${idx + 1}-stake`}>{asPercent(g.stake)}</TableCell>
               <TableCell data-testid={`guardian-${idx + 1}-voted`}>{g.voted ? 'Yes' : 'No'}</TableCell>
               {onGuardianSelect && (
