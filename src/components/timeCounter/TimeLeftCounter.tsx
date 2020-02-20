b626b3fc28ba7cfc25bc576ee5e3ccb65555fe23
Or Lavy <orlavyil@gmail.com>
2020-02-20 18:22:01 +0200

    Adds auto one second update to update the 'TimeLeftCounter'

--- src/components/timeCounter/TimeLeftCounter.tsx
+++ src/components/timeCounter/TimeLeftCounter.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import moment from 'moment';
+import { useNumber } from 'react-hanger';
+import useInterval from 'use-interval';
 
 interface IProps {
   toTimestamp: number;
 }
 
 export const TimeLeftCounter = React.memo<IProps>(props => {
+  // DEV_NOTE : These two linea are responsible for re-rendering the component
+  const num = useNumber(0);
+  useInterval(() => num.setValue(Date.now), 1000);
+
   const toMoment = moment.unix(props.toTimestamp).utc();
 
   return <>{toMoment.fromNow(true)}</>;
