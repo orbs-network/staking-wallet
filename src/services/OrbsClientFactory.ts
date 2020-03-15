85bf1997a7ffed4c54184d02c86b70c53c69b86c
Or Lavy <orlavyil@gmail.com>
2020-03-15 13:39:17 +0200

    Changes the orbs node protocol to https

--- src/services/OrbsClientFactory.ts
+++ src/services/OrbsClientFactory.ts
@@ -3,7 +3,8 @@ import { LocalSigner } from 'orbs-client-sdk';
 
 const ORBS_VIRTUAL_CHAIN_ID = 1_100_000; // The virtual chain Id on the Orbs network
 const ORBS_NODE_ADDRESS = '18.197.127.2'; // The Orbs node that we will query
-const ORBS_NODE_URL = `http://${ORBS_NODE_ADDRESS}/vchains/${ORBS_VIRTUAL_CHAIN_ID.toString()}`;
+const PROTOCOL = 'https';
+const ORBS_NODE_URL = `${PROTOCOL}://${ORBS_NODE_ADDRESS}/vchains/${ORBS_VIRTUAL_CHAIN_ID.toString()}`;
 
 export function BuildOrbsClient(): Client {
   const { publicKey, privateKey } = createAccount();
