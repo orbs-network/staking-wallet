cdffdec38c15449148a1c870f87f074bee4a990f
Or Lavy <orlavyil@gmail.com>
2020-03-15 14:18:26 +0200

    Changes the orbs node to use DNS name instead of https

--- src/services/OrbsClientFactory.ts
+++ src/services/OrbsClientFactory.ts
@@ -2,7 +2,7 @@ import { Client, createAccount, NetworkType } from 'orbs-client-sdk';
 import { LocalSigner } from 'orbs-client-sdk';
 
 const ORBS_VIRTUAL_CHAIN_ID = 1_100_000; // The virtual chain Id on the Orbs network
-const ORBS_NODE_ADDRESS = '18.197.127.2'; // The Orbs node that we will query
+const ORBS_NODE_ADDRESS = 'validator.orbs.com'; // The Orbs node that we will query
 const PROTOCOL = 'https';
 const ORBS_NODE_URL = `${PROTOCOL}://${ORBS_NODE_ADDRESS}/vchains/${ORBS_VIRTUAL_CHAIN_ID.toString()}`;
 
