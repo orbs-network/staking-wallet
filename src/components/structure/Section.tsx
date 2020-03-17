aa9692d63830dce644a10138fdd0f412ffb17191
Or Lavy <orlavyil@gmail.com>
2020-03-17 12:34:29 +0200

    Fixes 'section' direction to 'column'

--- src/components/structure/Section.tsx
+++ src/components/structure/Section.tsx
@@ -9,7 +9,7 @@ const StyledGrid = styled(Grid)({
 
 export const Section: React.FC = ({ children, ...otherProps }) => {
   return (
-    <StyledGrid item container {...otherProps}>
+    <StyledGrid item container direction={'column'} {...otherProps}>
       {children}
     </StyledGrid>
   );
