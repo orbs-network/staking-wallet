28354688ffc3ca2d3afdf7e69524d8476f86c370
Or Lavy <orlavyil@gmail.com>
2020-03-23 18:08:01 +0200

    Removes bloated flags library

--- src/components/nav/LanguagesSelector.tsx
+++ src/components/nav/LanguagesSelector.tsx
@@ -5,11 +5,11 @@
  * This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
  * The above notice should be included in all copies or substantial portions of the software.
  */
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { ChangeLangLink } from '../../multi-lang/ChangeLangLink';
 import Grid from '@material-ui/core/Grid';
 import styled from 'styled-components';
-import FlagIcon from '../icons/FlagIcon';
+import Icon from '@material-ui/core/Icon';
 
 const ButtonChangeLangLink = styled(ChangeLangLink)(({ theme }) => ({
   '&:hover': {
@@ -19,22 +19,37 @@ const ButtonChangeLangLink = styled(ChangeLangLink)(({ theme }) => ({
   fontSize: theme.typography.body1.fontSize,
 }));
 
+// TODO : FUTURE : O.L : Fix the country icons styling (notice that is is being wrapped in an 'a' element)
+//        + Maybe find a a better way to import the images
+const styleForIcon: CSSProperties = {
+  textAlign: 'center',
+};
+const CountrySvgImg = styled('img')(({ theme }) => ({
+  height: '0.75em',
+}));
+
 export const LanguagesSelector = React.memo(props => {
   return (
     <Grid container alignItems={'center'} spacing={2}>
       <Grid item>
         <ButtonChangeLangLink lang='en'>
-          <FlagIcon code={'us'} />
+          <Icon style={styleForIcon}>
+            <CountrySvgImg src='/assets/countryIcons/us.svg' />
+          </Icon>
         </ButtonChangeLangLink>
       </Grid>
       <Grid item>
         <ButtonChangeLangLink lang='jp'>
-          <FlagIcon code={'jp'} />
+          <Icon style={styleForIcon}>
+            <CountrySvgImg src='/assets/countryIcons/jp.svg' />
+          </Icon>
         </ButtonChangeLangLink>
       </Grid>
       <Grid item>
-        <ButtonChangeLangLink lang='ko'>
-          <FlagIcon code={'kr'} />
+        <ButtonChangeLangLink lang='kr'>
+          <Icon style={styleForIcon}>
+            <CountrySvgImg src='/assets/countryIcons/kr.svg' />
+          </Icon>
         </ButtonChangeLangLink>
       </Grid>
     </Grid>
