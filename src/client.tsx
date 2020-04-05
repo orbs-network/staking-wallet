19644f7ca7a2b25d6160fc974ddd33d5c3f465b4
Or Lavy <orlavyil@gmail.com>
2020-04-05 23:57:54 +0300

    Adds an ugly hack to fall gracfully with unsupported browsers

--- src/client.tsx
+++ src/client.tsx
@@ -3,7 +3,11 @@ import * as ReactDOM from 'react-dom';
 import { AppWrapper } from './AppWrapper';
 import { UnsopportedBrowserDisplay } from './UnsopportedBrowserDisplay';
 
-if (BigInt) {
+// @ts-ignore
+const bigIntSupported = BigInt(1) !== 'unsupported';
+const isSupported = bigIntSupported;
+
+if (isSupported) {
   ReactDOM.render(<AppWrapper />, document.getElementById('app'));
 } else {
   ReactDOM.render(<UnsopportedBrowserDisplay />, document.getElementById('app'));
