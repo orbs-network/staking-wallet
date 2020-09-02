a8e51ecdcc003cdc05aa43807b3d641c7c2ab179
Or Lavy <orlavyil@gmail.com>
2020-09-02 16:50:59 +0200

    Removes comments from status fetch

--- src/services/v2/orbsNodeService/OrbsNodeService.ts
+++ src/services/v2/orbsNodeService/OrbsNodeService.ts
@@ -17,8 +17,9 @@ export class OrbsNodeService implements IOrbsNodeService {
     const nodeUrl = nodeAddress || this.defaultNodeUrl;
     const model = new Model();
     // TODO : ORL : Fix CORS and then return this read.
-    // const rootNodeData = await fetchJson(`${nodeUrl}${ManagementStatusSuffix}`);
-    const rootNodeData = (RootNodeData as any) as IRootNodeData;
+    const rootNodeData = await fetchJson(`${nodeUrl}${ManagementStatusSuffix}`);
+    console.log(rootNodeData);
+    // const rootNodeData = (RootNodeData as any) as IRootNodeData;
     updateModel(model, rootNodeData);
 
     return {
