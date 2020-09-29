927130dc14e9100d8e1bded6a5c58e91d7cfd052
Or Lavy <orlavyil@gmail.com>
2020-09-29 12:28:37 +0200

    Adds gh-pages deployment

--- src/config.ts
+++ src/config.ts
@@ -54,10 +54,10 @@ if (process.env.NODE_ENV !== 'production') {
 
     config.ETHEREUM_PROVIDER_WS = 'ws://localhost:8545';
 
+    // config.contractsAddressesOverride.votingContract = VotingContractJSON.networks['5777'].address;
     config.contractsAddressesOverride.stakingContract = addresses.staking;
     config.contractsAddressesOverride.erc20Contract = addresses.erc20;
     config.contractsAddressesOverride.guardiansContract = addresses.guardians;
-    // config.contractsAddressesOverride.votingContract = VotingContractJSON.networks['5777'].address;
     config.contractsAddressesOverride.orbsRewardsDistributionContract = addresses.rewards;
     config.contractsAddressesOverride.delegationsContract = addresses.delegations;
     config.contractsAddressesOverride.committeeContract = addresses.commitee;
@@ -68,12 +68,28 @@ if (process.env.NODE_ENV !== 'production') {
 
 // TODO : ORL : Adjusts these addresses for v2.
 if (ethereumNetwork === 'ropsten') {
-  config.contractsAddressesOverride.stakingContract = '0x88287444f10709f9531D11e08DCd692deccd1d63';
-  config.contractsAddressesOverride.erc20Contract = '0xeD0Aa9A4F9e5ae9092994f4B86F6AAa89944939b';
-  config.contractsAddressesOverride.guardiansContract = '0x636315bcD912B1DbFe38E6b75f5B6AEE4Cd63B30';
-  // config.contractsAddressesOverride.votingContract = '0xF90a738CA659Fe99E357cB7F47Aaa5cB9b5724a2';
-  // TODO : ORL : Add Ropsten address
-  config.contractsAddressesOverride.orbsRewardsDistributionContract = '';
+  // config.contractsAddressesOverride.stakingContract = '0x88287444f10709f9531D11e08DCd692deccd1d63';
+  // config.contractsAddressesOverride.erc20Contract = '0xeD0Aa9A4F9e5ae9092994f4B86F6AAa89944939b';
+  // config.contractsAddressesOverride.guardiansContract = '0x636315bcD912B1DbFe38E6b75f5B6AEE4Cd63B30';
+  // // config.contractsAddressesOverride.votingContract = '0xF90a738CA659Fe99E357cB7F47Aaa5cB9b5724a2';
+  // // TODO : ORL : Add Ropsten address
+  // config.contractsAddressesOverride.orbsRewardsDistributionContract = '';
+  //
+  // config.earliestBlockForDelegationOverride = 0; // Local env starts from 0.
+
+  const ROPSTEN_ERC20 = '0xD2Aad026e9a307Df950143B5ec841a2388378f8F';
+  const ROPSTEN_DELEGATIONS = '0x44c46e661424Dd0db06570087Fe19ee364Eb3296';
+  const ROPSTEN_STAKING = '0x0aB60194B38BcE3F2A05A46D49e8d759c1bd460d';
+  const ROPSTEN_REWARDS = '0x8eC056cdcd5b41Dc581b6B7d4F555F5Dac056C7c';
+  const ROPSTEN_GUARDIANS = '';
+  const ROPSTEN_COMMITTEE = '';
+
+  config.contractsAddressesOverride.stakingContract = ROPSTEN_STAKING;
+  config.contractsAddressesOverride.erc20Contract = ROPSTEN_ERC20;
+  config.contractsAddressesOverride.guardiansContract = ROPSTEN_GUARDIANS;
+  config.contractsAddressesOverride.orbsRewardsDistributionContract = ROPSTEN_REWARDS;
+  config.contractsAddressesOverride.delegationsContract = ROPSTEN_DELEGATIONS;
+  config.contractsAddressesOverride.committeeContract = ROPSTEN_COMMITTEE;
 
   config.earliestBlockForDelegationOverride = 0; // Local env starts from 0.
 }
