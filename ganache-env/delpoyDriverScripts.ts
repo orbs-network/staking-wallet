b9ea9f4b2871a710caa0cf8dfd755991ab3a4538
Or Lavy <orlavyil@gmail.com>
2020-11-03 14:11:20 +0200

    Update deploy driver script to be more comprehensive

--- ganache-env/delpoyDriverScripts.ts
+++ ganache-env/delpoyDriverScripts.ts
@@ -6,6 +6,7 @@
 import { toWei } from 'web3-utils';
 import { Driver as OrbsV2Driver } from '@orbs-network/orbs-ethereum-contracts-v2';
 import { writeFileSync } from 'fs';
+import { Participant } from '@orbs-network/orbs-ethereum-contracts-v2/release/test/driver';
 
 const co = require('@orbs-network/orbs-ethereum-contracts-v2');
 const BN = require('bn.js');
@@ -13,7 +14,7 @@ const BN = require('bn.js');
 const deployDriverScripts = async () => {
   try {
     console.log('deploying Orbs PoS V2 contracts');
-    const driver = await OrbsV2Driver.new();
+    const driver = await OrbsV2Driver.new({ maxCommitteeSize: 22 });
     console.log('After deploying Orbs PoS V2 contracts');
 
     const orbsV2Account = driver.accounts[0];
@@ -24,11 +25,16 @@ const deployDriverScripts = async () => {
     console.log(`Balance of ${orbsV2Account} `, await driver.erc20.balanceOf(orbsV2Account));
 
     // Setting delegators staking rewards
-    // const rewardsInWeiBN = toWei(new BN(5_000_000));
-    // await driver.erc20.approve(driver.stakingRewards.address, rewardsInWeiBN);
-    // await driver.stakingRewards.acceptRewardsBalanceMigration(orbsV2Account, 0, rewardsInWeiBN, {
-    //   from: orbsV2Account,
-    // });
+    const rewardsInWeiBN = toWei(new BN(5_000));
+    await driver.erc20.approve(driver.stakingRewards.address, rewardsInWeiBN);
+    await driver.stakingRewards.acceptRewardsBalanceMigration(orbsV2Account, 0, rewardsInWeiBN, {
+      from: orbsV2Account,
+    });
+
+    // set rewards
+    await driver.stakingRewards.setAnnualStakingRewardsRate(12000, toWei(new BN(80_000_000)), {
+      from: driver.functionalManager.address,
+    });
 
     const addresses = {
       staking: driver.staking.address,
@@ -42,6 +48,16 @@ const deployDriverScripts = async () => {
     console.log('Saving addresses to file');
     writeFileSync('./_out/addresses.json', JSON.stringify(addresses, null, 2));
     writeFileSync('../src/local/addresses.json', JSON.stringify(addresses, null, 2));
+
+    // Create a committee
+    const committee: Array<Participant> = [];
+    for (let i = 0; i < 4; i++) {
+      const { v } = await driver.newGuardian(toWei(new BN(10_000_000)), false, false, true);
+      committee[i] = v;
+      console.log('Committee member:', committee[i].address);
+    }
+    // delegate to committee member 0
+    await driver.delegations.delegate(committee[0].address, { from: orbsV2Account });
   } catch (e) {
     console.log('error');
     console.error(e);
