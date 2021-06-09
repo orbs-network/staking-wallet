7ff406b5dbed8aa17722dbdc6679e5cb1e78a71e
Itamar Arjuan <itamar.arjuan@gmail.com>
2021-06-09 14:35:55 +0300

    removing sudo , should now work without requiring sudo for docker commands

--- ganache-env/deploy.sh
+++ ganache-env/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-aws ecr get-login-password --region us-east-2 | sudo docker login --username AWS --password-stdin 506367651493.dkr.ecr.us-east-2.amazonaws.com
-sudo docker pull 506367651493.dkr.ecr.us-east-2.amazonaws.com/management-service-ropsten:latest
-sudo docker-compose -f docker-compose-remote.yml down
-sudo docker-compose -f docker-compose-remote.yml up -d
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 506367651493.dkr.ecr.us-east-2.amazonaws.com
+docker pull 506367651493.dkr.ecr.us-east-2.amazonaws.com/management-service-ropsten:latest
+docker-compose -f docker-compose-remote.yml down
+docker-compose -f docker-compose-remote.yml up -d
