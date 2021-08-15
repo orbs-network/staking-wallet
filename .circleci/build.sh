69fcea497eaa06f5382d7d5ce8fc6d1a5818777e
David Dayag <daviddayag@gmail.com>
2021-08-15 13:30:59 +0300

    Update build.sh
    should work now

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,6 +3,5 @@
 git config --global user.email "itamar@orbs.com"
 git config --global user.name "Itamar Arjuan"
 
-# brakes ci, TODO fix
-# npm install
-# npm run deploy-staging
+npm install
+npm run deploy-staging
