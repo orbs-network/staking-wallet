35479d2371b3a838671201180f8eb6e1d47cb6af
David Dayag <daviddayag@gmail.com>
2021-08-15 16:07:10 +0300

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,17 +3,5 @@
 git config --global user.email "itamar@orbs.com"
 git config --global user.name "Itamar Arjuan"
 
-pwd
-
-echo
-
-ls
-
-echo
-
-
-npm --version
-node --version
-
 npm install
 npm run deploy-staging
