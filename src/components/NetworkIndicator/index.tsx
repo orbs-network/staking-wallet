88f210035d12c6dd4273b5ede760814ab685cf9b
Denisn <denis.nikitin@gotech.io>
2021-08-16 19:06:17 +0300

    added new env file .env.staging, to test the prod without using starting analytics, changed the popup to the new design

--- src/components/NetworkIndicator/index.tsx
+++ src/components/NetworkIndicator/index.tsx
@@ -1,7 +1,9 @@
-import { Typography } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 import React from 'react';
 import useNetwork from '../hooks/useNetwork';
 import { useNetworkIndicatorStyles } from './styles';
+import LinkOffIcon from '@material-ui/icons/LinkOff';
+import { makeStyles } from '@material-ui/core/styles';
 
 const NetworkIndicator = () => {
   const [network, chainId] = useNetwork();
@@ -10,12 +12,14 @@ const NetworkIndicator = () => {
     return <div className={classes.devContainer}>{network}</div>;
   }
 
-  if (chainId && chainId !== process.env.CHAIN_ID) {
+  if (chainId && chainId !== process.env.TARGET_NETWORK) {
     return (
       <div className={classes.prodContainer}>
         <section className={classes.overlay}></section>
         <div className={classes.prodContainerContent}>
-          <Typography>Please connect to Mainnet</Typography>
+          <Typography className={classes.title}>Oops, wrong network!</Typography>
+          <LinkOffIcon className={classes.icon} />
+          <Typography className={classes.text}>Please change the network to Ethereum Mainnet </Typography>
         </div>
       </div>
     );
