a55becd6edce6dec5926e2c175324491cf533c49
denis-mixin <denisn@mixin.co.il>
2021-08-17 16:10:52 +0300

    fixed, now only actionable components gave hover in balance card

--- src/components/balance-card/index.tsx
+++ src/components/balance-card/index.tsx
@@ -3,7 +3,6 @@ import { Button, Grid, Typography, Tooltip } from '@material-ui/core';
 import { CommonDivider } from '../base/CommonDivider';
 import { CommonActionButton } from '../base/CommonActionButton';
 import { useStringOrElement } from '../hooks/commonHooks';
-import useHover from '@react-hook/hover';
 import BaseLoader from '../loaders';
 import Loaders from '../loaders/loader-components/index';
 import BalanceCardCounters from './components/counters';
@@ -48,30 +47,24 @@ const BalanceCard = (props: IProps) => {
     isLoading,
   } = props;
 
-  const hoverTargetRef = useRef();
-  const isHovering = useHover(hoverTargetRef);
   const titleElement = useStringOrElement(title);
   const hasMainButton = actionButtonTitle || onActionButtonPressed;
   const hasSecondaryActionButton = secondaryActionButtonTitle || onSecondaryActionButtonPressed;
 
   return (
-    <StyledGrid container direction={'column'} data-testid={balanceCardTestId} ref={hoverTargetRef}>
+    <StyledGrid container direction={'column'} data-testid={balanceCardTestId}>
       <BaseLoader isLoading={isLoading} LoaderComponent={Loaders.BalanceCard}>
         <>
           <Grid item container alignItems={'center'} justify={'space-between'} style={{ height: '2rem' }}>
             <Grid item>
-              <Typography variant={'body1'}>{titleElement}</Typography>
+              <Typography className={classes.title} variant={'body1'}>
+                {titleElement}
+              </Typography>
             </Grid>
             {hasSecondaryActionButton && (
               <Grid item>
                 <Button
                   className={classes.secondaryActionButton}
-                  variant={isHovering ? 'outlined' : 'text'}
-                  style={{
-                    backgroundColor: isHovering ? 'rgba(33,33, 33, 1)' : '',
-                    paddingLeft: '10px',
-                    paddingRight: '10px',
-                  }}
                   color={'secondary'}
                   onClick={onSecondaryActionButtonPressed}
                   disabled={!secondaryActionButtonActive}
@@ -94,12 +87,7 @@ const BalanceCard = (props: IProps) => {
 
           {hasMainButton && (
             <Grid item>
-              <CommonActionButton
-                variant={isHovering ? 'outlined' : 'contained'}
-                fullWidth={true}
-                disabled={!actionButtonActive}
-                onClick={onActionButtonPressed}
-              >
+              <CommonActionButton fullWidth={true} disabled={!actionButtonActive} onClick={onActionButtonPressed}>
                 {actionButtonTitle}
               </CommonActionButton>
             </Grid>
